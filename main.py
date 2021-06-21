3c7742e62fbc0d7444b88117e205bdda59721ae1
mickey <mickeyking852@gmail.com>
2021-06-21 09:37:14 +0800

    uploaded in 21/06/2021-09:37

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ class Dict_generator:
 
     def generate(self,type:string,size:int,out:string)-> string:
         set = ''
-        inputer = open(out, 'w+')
+        inputer = open(out, 'a+')
 
         if type == 'emoji':
             set = self.emoji()
@@ -47,5 +47,61 @@ class Dict_generator:
 
         print(f'file size: {os.path.getsize(out)}\ntarget size:{size}')
 
+    def mix_generate(self,type:[],size:int,out:string):
+        set_1 = ''
+        set_2 = ''
+        inputer = open(out, 'a+')
+
+        if 'emoji' in type:
+            if set_1 == '':
+                set_1 = self.emoji()
+            elif set_2 == '':
+                set_2 = self.emoji()
+
+        if 'english' in type:
+            if set_1 == '':
+                set_1 = self.english()
+            elif set_2 == '':
+                set_2 = self.english()
+
+        if 'chinese' in type:
+            if set_1 == '':
+                set_1 = self.chinese()
+            elif set_2 == '':
+                set_2 = self.chinese()
+
+        #part_1
+        while os.path.getsize(out) <= size*0.5:
+            inputer.write(set_1[random.randint(0, len(set_1) - 1)])
+            inputer.close()
+            inputer = open(out, 'a+')
+
+        #part_2
+        while os.path.getsize(out) <= size*0.5:
+            inputer.write(set_2[random.randint(0, len(set_2) - 1)])
+            inputer.close()
+            inputer = open(out, 'a+')
+
+    def mix_genrate_2(self,type:[],size:int,out:string):
+        set = ''
+        inputer = open(out, 'a+')
+
+        if 'emoji' in type:
+            set += self.emoji()
+
+        if 'english' in type:
+            set += self.english()
+
+        if 'chinese' in type:
+            set += self.chinese()
+
+        while os.path.getsize(out) <= size:
+
+            inputer.write(set[ random.randint(0, len(set)-1) ])
+            inputer.close()
+            inputer = open(out, 'a+')
+
+        print(f'file size: {os.path.getsize(out)}\ntarget size:{size}')
+
 obj = Dict_generator()
-#obj.generate('english',12,'/home/mickey/Desktop/test.txt')
+
